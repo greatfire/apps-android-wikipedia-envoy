f0afc62024a7aa756ac355e019ad143da450acf2
Steven McDonald <steven.mcdonald@gmail.com>
2025-01-28 13:14:24 -0800

    changes for plenipotentiary

--- app/src/main/java/org/wikipedia/main/MainActivity.kt
+++ app/src/main/java/org/wikipedia/main/MainActivity.kt
@@ -438,7 +438,6 @@ class MainActivity : SingleFragmentActivity<MainFragment>(), MainFragment.Callba
             this@MainActivity,
             listOfUrls,
             DIRECT_URL,
-            Secrets().gethystCert(shortPackage),
             urlSources,
             urlInterval,
             urlStart,
